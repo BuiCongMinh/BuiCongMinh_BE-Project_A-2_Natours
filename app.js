249427aed7b8e3fdc9b0db0caa30d0dd7a7abd09
BuiCongMinh <minhvnvnvn1998@gmail.com>
2023-03-05 10:32:54 +0700

    hoàn thành file 09 chap 114 Better Errors and Refactoring (tao classs AppError ke thua lop Error va tai cau truc hàm su ly loi tren file app.js {tach phan handle cua ham xu ly loi app.use (err,req,res,next) ra mot file errorController.js})

--- app.js
+++ app.js
@@ -1,53 +1,41 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const tourRouter = require('./routes/tourRouter')
-const userRouter = require('./routes/userRouter')
+const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controlles/errorController');
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
 
 // 1) MIDDLEWERE
 if (process.env.NODE_ENV === 'develoment') {
     app.use(morgan("dev"))
-}
+};
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.static(`${__dirname}/after-section-06/public`));
 
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next()
-})
+});
 
 
 // cách 2
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/user', userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/user', userRouter);
 
 
 //Các router có đường url ko xác định sẽ được truyền vào đây !
-app.all('*',(req,res,next)=>{   // app.all là chỉ tất cả các phương thức restAPI
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on this server ! `
-    // })
+app.all('*', (req, res, next) => {   // app.all là chỉ tất cả các phương thức restAPI
 
-    const err = new Error(`Cant find ${req.originalUrl} on this server !`) ;   // req.originalUrl chính là định nghĩa url của router
-    err.status = 'fail';
-    err.statusCode = 404;
-    next(err);
-})
+    next(new AppError(`Cant find ${req.originalUrl} on this server ! `));
+}) 
 
 
 //Tất cả lỗi trên server sẽ được đổ vào hàm này !
-app.use((err, req, res, next )=>{
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-     
-    res.status(err.statusCode).json({
-        status: err.statusCode,
-        message: err.message
-    })
-})
+app.use(globalErrorHandler);
 
 
 module.exports = app;
