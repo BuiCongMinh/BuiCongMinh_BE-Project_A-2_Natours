28cd9c509d2bae1c2d850be9cfbba5b329633fe5
BuiCongMinh <minhvnvnvn1998@gmail.com>
2023-03-05 18:15:26 +0700

    hoàn thành file 09 chap 117 Errors During Development vs Production (vè steup lôix đoi vơi ben dev và vơis ben production )

--- controlles/errorController.js
+++ controlles/errorController.js
@@ -1,11 +1,48 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+    
+    //Operational, trusted error: send message to client (Những lỗi vận hành (operational errors) mà chúng ta có thể gửi thông tin chi tiết được cho bên phía khách hàng ! )
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    }
+
+    //Programming or other unknown error: don't leak error details (Những lối ko xác định và chúng ta ko muốn cho khách hàng biết về chi tiết (như là sử dụng bên code thứ ba hay là chi tiết về code rong app!))  
+    else{
+        // 1) Log error 
+        console.error('>>> ERROR !!!', err);  //console.error giống với console.log nhưng sẽ cụ thể hơn với các lỗi error
+
+        // 2) send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong !'
+        })
+    }
+
+}
+
 module.exports = (err, req, res, next) => {
     // console.log('>>> check stack:',err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    //sẽ chia ra chạy bên phía 'development' bên nhà phát triển ứng dụng (bên dev)
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    };
+
 }
