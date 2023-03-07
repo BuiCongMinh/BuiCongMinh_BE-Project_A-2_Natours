eef746594bfc1322549086349e8cdda44d2b7ceb
BuiCongMinh <minhvnvnvn1998@gmail.com>
2023-03-07 14:21:17 +0700

    hoàn thành file 09 chap 119 Handling Duplicate Database Fields (nói vèe sư ly loi trung lap ten name . Vi đay la loi ben mongoose nên chung ta muôn in ra lôi cho ben khach hang là ten đã chùng lap nhu tren postman . Chung ta se dua vao ma code để làm dieu do )

--- controlles/errorController.js
+++ controlles/errorController.js
@@ -1,7 +1,14 @@
 const AppError = require("../utils/appError");
 
 const handleCastErrorDB = err => {
-    const message =  `Invalid ${err.path}: ${err.value}. `;
+    const message = `Invalid ${err.path}: ${err.value}. `;
+    return new AppError(message, 400);
+}
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/);
+    // console.log('>>> Check value: ', value);
+    const message = ` Duplicate field value: ${value}. Please use another value !`;
+    
     return new AppError(message, 400);
 }
 
@@ -15,7 +22,7 @@ const sendErrorDev = (err, res) => {
 }
 
 const sendErrorProd = (err, res) => {
-    
+
     //Operational, trusted error: send message to client (Những lỗi vận hành (operational errors) mà chúng ta có thể gửi thông tin chi tiết được cho bên phía khách hàng ! )
     if (err.isOperational) {
         res.status(err.statusCode).json({
@@ -25,7 +32,7 @@ const sendErrorProd = (err, res) => {
     }
 
     //Programming or other unknown error: don't leak error details (Những lối ko xác định và chúng ta ko muốn cho khách hàng biết về chi tiết (như là sử dụng bên code thứ ba hay là chi tiết về code rong app!))  
-    else{
+    else {
         // 1) Log error 
         console.error('>>> ERROR !!!', err);  //console.error giống với console.log nhưng sẽ cụ thể hơn với các lỗi error
 
@@ -48,10 +55,13 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     }
-    else if (process.env.NODE_ENV === 'production') {
-        let error = {... err};
 
-        if(error.name === 'CastError') error = handleCastErrorDB(error);
+    //Chạy bên phía 'production '
+    else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
         sendErrorProd(error, res);
     };
